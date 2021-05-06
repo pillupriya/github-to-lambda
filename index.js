38c627a0def47ede9a0f72019e9df1745ba869e5
Agnes Pious <agnes.pious@capgemini.com>
2021-05-06 20:40:31 +0530

    new test timestamp

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const { v4: uuidv4 } = require('uuid');
+const timestamp = require('time-stamp');
+
 exports.handler = function (event, context) {
     console.log('first build test');
     console.log(JSON.parse(event));
 	context.succeed('hello ' + event.name);
-    console.log("dependency added uuid "+uuidv4());
+    console.log('timestamp :'+timestamp());
 }
