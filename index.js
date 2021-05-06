8436cab2db680db5cf110eabf700e1e7709fecb7
Agnes Pious <agnes.pious@capgemini.com>
2021-05-06 20:30:38 +0530

    new msg

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const { uuid } = require('uuidv4');
+const { v4: uuidv4 } = require('uuid');
 exports.handler = function (event, context) {
     console.log('first build test');
-    console.log(event);
+    console.log(JSON.parse(event));
 	context.succeed('hello ' + event.name);
-    console.log("dependency added uuid "+uuid());
+    console.log("dependency added uuid "+uuidv4());
 }
