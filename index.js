20fe6cbfec75c71a2069a5b75285c19d6eaf5c95
Agnes Pious <agnes.pious@capgemini.com>
2021-05-06 20:11:26 +0530

    new build

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { uuid } = require('uuidv4');
 exports.handler = function (event, context) {
-    console.log('first build');
+    console.log('first build test');
     console.log(event);
 	context.succeed('hello ' + event.name);
     console.log("dependency added uuid "+uuid());
